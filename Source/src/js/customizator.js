eb9095fdad2e5cb8198123ed447b6b1f1bfdbe3a
Vivadis <vivadi23@mail.ru>
2021-03-06 08:21:30 +0300

    1x magnification of the text on the site

--- Source/src/js/customizator.js
+++ Source/src/js/customizator.js
@@ -1,13 +1,21 @@
 export default class Customizator {
     constructor() {
-        
-    }
-
-
-    render() {
         this.btnBlock = document.createElement('div');
         this.colorPicker = document.createElement('input');
 
+        this.btnBlock.addEventListener('click', (e) => this.onScaleChange(e));
+    }
+
+    onScaleChange(e) {
+        let scale;
+        const body = document.querySelector('body');
+        if (e.target.value) {
+            scale = e.target.value.replace(/x/g, "");
+        }
+        console.log(scale);
+    }
+
+    render() {
         let scaleInputS = document.createElement('input');
             scaleInputM = document.createElement('input');
             panel = document.createElement('div');
@@ -17,6 +25,14 @@ export default class Customizator {
         scaleInputS.classList.add('scale_btn');
         scaleInputM.classList.add('scale_btn');
         this.btnBlock.classList.add('scale');
+        this.colorPicker.classList.add('color');
+
+        scaleInputS.setAttribute('type', 'button');
+        scaleInputM.setAttribute('type', 'button');
+        scaleInputS.setAttribute('value', '1x');
+        scaleInputM.setAttribute('value', '1.5x');
+        this.colorPicker.setAttribute('type', 'color');
+        this.colorPicker.setAttribute('value', '#ffffff');
 
         this.btnBlock.append(scaleInputS, scaleInputM);
 
