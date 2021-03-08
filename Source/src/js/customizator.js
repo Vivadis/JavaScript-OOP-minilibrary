c1a6e19118b474d0caee1ea9674df7c56366b78c
Vivadis <vivadi23@mail.ru>
2021-03-08 07:42:54 +0300

    Customizing the Color Panel

--- Source/src/js/customizator.js
+++ Source/src/js/customizator.js
@@ -4,6 +4,7 @@ export default class Customizator {
         this.colorPicker = document.createElement('input');
 
         this.btnBlock.addEventListener('click', (e) => this.onScaleChange(e));
+        this.colorPicker.addEventListener('input', (e) => this.onColorChange(e));
     }
 
     onScaleChange(e) {
@@ -17,10 +18,12 @@ export default class Customizator {
             element.childNodes.forEach(node => {
                 if (node.nodeName === '#text' && node.nodeValue.replace(/\s+/g, "").length > 0){
                     
-                    if() {
+                    if(!node.parentNode.getAttribute('data-fz')) {
                         let value = window.getComputedStyle(node.parentNode, null).fontSize;
-                        node.parentNode.setAttribute()
-                        node.parentNode.style.fontSize = +value.replace(/px/g, "") * scale + "px";
+                        node.parentNode.setAttribute('data-fz', +value.replace(/px/g, ""))
+                        node.parentNode.style.fontSize = node.parentNode.getAttribute('data-fz') * scale + "px";
+                    } else {
+                        node.parentNode.style.fontSize = node.parentNode.getAttribute('data-fz') * scale + "px";
                     }
 
                     let value = window.getComputedStyle(node.parentNode, null).fontSize;
@@ -31,11 +34,61 @@ export default class Customizator {
             })
         }
         recursy(body);
+    }
 
-        console.log(scale);
+    onColorChange(e) {
+        const body = document.querySelector('body');
+        body.style.backgroundColor = e.target.value;
+        console.log(e.target.value);
+    }
+
+    injectStyle() {
+        const style = document.createElement('style');
+        style.innerHTML = `
+        .panel {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            position: fixed;
+            top: 10px;
+            right: 0;
+            border: 1px solid rgba(0,0,0, .2);
+            box-shadow: 0 0 20px rgba(0,0,0, .5);
+            width: 300px;
+            height: 60px;
+            background-color: #fff;
+        
+        }
+        
+        .scale {
+            display: flex;
+            justify-content: space-around;
+            align-items: center;
+            width: 100px;
+            height: 40px;
+            
+        }
+
+        .scale_btn {
+            display: block;
+            width: 40px;
+            height: 40px;
+            border: 1px solid rgba(0,0,0, .2);
+            border-radius: 4px;
+            font-size: 18px;
+        }
+        
+        .color {
+            width: 40px;
+            height: 40px;
+        }
+        `;
+        document.querySelector('head').appendChild(style);
     }
 
     render() {
+        this.injectStyle();
+
         let scaleInputS = document.createElement('input');
             scaleInputM = document.createElement('input');
             panel = document.createElement('div');
